1e4e1b44b295b3b422614024418503d8b4d3278f
chandangupta123 <66155483+chandangupta123@users.noreply.github.com>
2021-01-25 21:27:33 +0530

    Add files via upload

---
+++ backend.php
@@ -0,0 +1,66 @@
+
+
+    <?php
+include 'database.php';
+if(count($_POST)>0){
+	if($_POST['type']==1){
+		$name=$_POST['name'];
+		$date=$_POST['date'];
+		$sname=$_POST['searchname'];
+		$sql = "INSERT INTO `crud`( `name`, `date`,`sname`,) 
+		VALUES ('$name','$date','$sname')";
+		if (mysqli_query($conn, $sql)) {
+			echo json_encode(array("statusCode"=>200));
+		} 
+		else {
+			echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+		}
+		mysqli_close($conn);
+	}
+}
+if(count($_POST)>0){
+	if($_POST['type']==2){
+		$id=$_POST['id'];
+		$name=$_POST['name'];
+		$sname=$_POST['sname'];
+		$date=$_POST['date'];
+		$sql = "UPDATE `crud` SET `name`='$name',`email`='$sname',`phone`='$date', WHERE id=$id";
+		if (mysqli_query($conn, $sql)) {
+			echo json_encode(array("statusCode"=>200));
+		} 
+		else {
+			echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+		}
+		mysqli_close($conn);
+	}
+}
+if(count($_POST)>0){
+	if($_POST['type']==3){
+		$id=$_POST['id'];
+		$sql = "DELETE FROM `crud` WHERE id=$id ";
+		if (mysqli_query($conn, $sql)) {
+			echo $id;
+		} 
+		else {
+			echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+		}
+		mysqli_close($conn);
+	}
+}
+if(count($_POST)>0){
+	if($_POST['type']==4){
+		$id=$_POST['id'];
+		$sql = "DELETE FROM crud WHERE id in ($id)";
+		if (mysqli_query($conn, $sql)) {
+			echo $id;
+		} 
+		else {
+			echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+		}
+		mysqli_close($conn);
+	}
+}
+
+?>
+
+
